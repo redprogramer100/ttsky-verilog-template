0295f52819cd662c2061ceb05a58d43ac898038f
Richard Alfredo Tarqui Mamani <redprogramer100@gmail.com>
2026-04-18 19:22:13 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,35 @@
-`default_nettype none
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
 module tb ();
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+    // Entradas / salidas del DUT
+    reg  clk;
+    reg  rst_n;
+    reg  [7:0] ui_in;
+    reg  [7:0] uio_in;
+    wire [7:0] uo_out;
+    wire [7:0] uio_out;
+    wire [7:0] uio_oe;
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+    // Instancia del DUT
+    tt_um_Richard_Tarqui_contador_uart_simple dut (
+        .ui_in   (ui_in),
+        .uo_out  (uo_out),
+        .uio_in  (uio_in),
+        .uio_out (uio_out),
+        .uio_oe  (uio_oe),
+        .clk     (clk),
+        .rst_n   (rst_n)
+    );
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+    // Clock 50 MHz → periodo 20 ns
+    initial clk = 0;
+    always #10 clk = ~clk;
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+    // Volcado de formas de onda
+    initial begin
+        $dumpfile("tb.fst");
+        $dumpvars(0, tb);
+    end
 
 endmodule
